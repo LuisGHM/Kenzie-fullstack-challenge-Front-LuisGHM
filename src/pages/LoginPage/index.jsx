7bdd96766c7ca9eea273129e426e4c59887c314f
Luis <lgmarchioreh@gmail.com>
2024-01-29 17:37:25 -0300

    FEAT(pages): Added Login, Register, and Home pages

---
+++ src/pages/LoginPage/index.jsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "../../components/form/LoginForm"
+
+export const LoginPage = () => {
+    return(
+        <>
+            <main>
+                <LoginForm/>
+            </main>
+        </>
+    )
+}
