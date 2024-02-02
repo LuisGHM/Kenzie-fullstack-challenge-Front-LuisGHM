9b3255efce08c2271659a577d01e4e8a96a65f94
Luis <lgmarchioreh@gmail.com>
2024-02-02 15:53:28 -0300

    FEAT(auth): Protected home page route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,13 +3,16 @@ import { LoginPage } from "../pages/LoginPage";
 import { RegisterPage } from "../pages/RegisterPage";
 import { HomePage } from "../pages/HomePage";
 import { ErrorPage } from "../pages/ErrorPage";
+import { ProtectedRoutes } from "../components/ProtectRoutes";
 
 export const RoutesMain = () => {
   return (
     <Routes>
         <Route path="/" element={<LoginPage/>} />
         <Route path="/register" element={<RegisterPage/>} />
-        <Route path="/home" element={<HomePage/>} />
+        <Route path="/home" element={<ProtectedRoutes/>}>
+          <Route index element={<HomePage/>} /> 
+        </Route>
         <Route path="*" element={<ErrorPage/>}/>
     </Routes>
   );
